f988ace05d84774d16525bd0e867824f465c31de
walkerturbs <walker.turbyfill@gmail.com>
2025-03-19 23:40:11 -0400

    lots of updates

--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 #!/bin/zsh
 
 # Apple script to turn on dark mode
-osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to dark mode'
+osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'
 
 # Taking inputs for later github setup
 read -p "Enter your github username: " username
 read -p "Enter your email: " email
-
-# Install Oh My Zsh silently (avoid breaking the script)
-RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+read -p "Is this a work laptop (y/n) " work
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -18,10 +16,14 @@ export PATH="/opt/homebrew/bin:$PATH"
 source ~/.zshrc
 
 # install brew casks
-xargs brew install --cask < brew-casks.txt
+cat brew-casks.txt | xargs brew install --cask
 
 # install brew formulae
-xargs brew install --formulae < brew-formulae.txt
+cat brew-formulae.txt | xargs brew install --formulae
+
+# Install Oh My Zsh silently (avoid breaking the script)
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source ~/.zshrc
 
 ### Git setup
 # Add username and email for github
@@ -32,8 +34,32 @@ git config --global init.defaultBranch main
 # Set pull to repase to maintain a linear history 
 git config --global pull.rebase true
 
+# Download anonymous pro font
+wget -P ~/Downloads https://www.marksimonson.com/assets/content/fonts/AnonymousProMinus-1_003.zip
+
+# Unzip the font files to the font library
+unzip -j ~/Downloads/AnonymousProMinus-1_003.zip "AnonymousProMinus-1_003/Anonymous Pro Minus B.ttf" "AnonymousProMinus-1_003/Anonymous Pro Minus BI.ttf" "AnonymousProMinus-1_003/Anonymous Pro Minus I.ttf" "AnonymousProMinus-1_003/Anonymous Pro Minus.ttf" -d ~/Library/Fonts/
+
+# Cleanup and delete the downloaded zip files
+rm ~/Downloads/AnonymousProMinus-1_003.zip
+
+echo 'eval "$(uv generate-shell-completion zsh)"' >> ~/.zshrc
+echo 'eval "$(uvx --generate-shell-completion zsh)"' >> ~/.zshrc
+
 # Open Tabs for chrome and gcal and gmail for adding to dock
 open -a Safari 'https://mail.google.com' 'https://calendar.google.com'
 
-# Install uv package
-pip install uv
+# If work then also open tab for gcloud sdk
+if [[ "$work" == "Y" || "$work" == "y" ]]; then
+    open -a Safari "https://cloud.google.com/sdk/docs/install-sdk"
+fi 
+
+# Add vscode to path
+cat << EOF >> ~/.zprofile
+# Add Visual Studio Code (code)
+export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+EOF
+source ~/.zshrc
+
+
+cat vs_code_extensions_list.txt | xargs -L1 code --install-extension
